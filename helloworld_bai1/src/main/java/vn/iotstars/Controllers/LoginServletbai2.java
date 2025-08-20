37daf34d4be8f38f846cedc3d4a7be3113690294
LamVanDi <lamvandi11@gmail.com>
2025-08-20 21:54:01 +0700

    Update LoginServletbai2.java

--- helloworld_bai1/src/main/java/vn/iotstars/Controllers/LoginServletbai2.java
+++ helloworld_bai1/src/main/java/vn/iotstars/Controllers/LoginServletbai2.java
@@ -14,6 +14,8 @@ import jakarta.servlet.http.HttpSession;
 public class LoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
