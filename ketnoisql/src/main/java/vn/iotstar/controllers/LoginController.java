3cd08dfd839b963e6cb779b2f062e2eb9c61e97a
LamVanDi <lamvandi11@gmail.com>
2025-08-27 15:58:45 +0700

    Update LoginController.java

--- ketnoisql/src/main/java/vn/iotstar/controllers/LoginController.java
+++ ketnoisql/src/main/java/vn/iotstar/controllers/LoginController.java
@@ -15,7 +15,7 @@ import vn.iotstar.service.UserService;
 import vn.iotstar.service.impl.UserServiceImpl;
 
 @SuppressWarnings("serial")
-@WebServlet("/login")
+@WebServlet(urlPatterns = "/login")
 public class LoginController extends HttpServlet {
 
 	public static final String SESSION_USERNAME = "username";
@@ -23,6 +23,7 @@ public class LoginController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession(false);
+
 
         if (session != null && session.getAttribute("account") != null) {
             User user = (User) session.getAttribute("account");
@@ -49,7 +50,6 @@ public class LoginController extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
 		resp.setContentType("text/html;charset=UTF-8");
         req.setCharacterEncoding("UTF-8");
 
@@ -77,8 +77,8 @@ public class LoginController extends HttpServlet {
             resp.getWriter().println("<h3>Đăng nhập thành công! Xin chào: " + user.getUserName() + "</h3>");
         } else {
             req.setAttribute("alert", "Tài khoản hoặc mật khẩu không đúng");
-		}
-
+            req.getRequestDispatcher("/view/login.jsp").forward(req, resp);
+	    }
 	}
 	private void saveRemeberMe(HttpServletResponse response, String username) {
 		Cookie cookie = new Cookie(COOKIE_REMEMBER, username);
